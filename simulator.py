321fdf8a040f54775a93efa0043dc399d9150dbb
jmelnich <imelnych@e2r12p8.unit.ua>
2018-06-04 22:12:09 +0300

    final

--- simulator.py
+++ simulator.py
@@ -17,7 +17,6 @@ def mirror(servers_quantity):
 		servers.append(row)
 	return servers
 
-
 def randomize(servers_quantity):
 	servers = mirror(servers_quantity)
 	i = 0
@@ -33,15 +32,57 @@ def randomize(servers_quantity):
 			flat.append(item)
 	cpp_1 = random.sample(flat,len(flat))
 	cpp_2 = random.sample(flat,len(flat))
-	altogether = cpp_1 + cpp_2
-	print(altogether)
 
+	altogether = []
+	k = 0
+	i = 0
+	while (i < servers_quantity - 1):
+		j = 0
+		altogether.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+		altogether.append([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+		while (j < 10):
+			altogether[i][j] = cpp_1[k]
+			altogether[i + 1][j] = cpp_2[k]
+			k += 1
+			j += 1
+		i += 2
+	return(altogether)
 
+def checkFragment(servers, dead_server, fragment):
+	row = 0
+	while (row < 10):
+		if (arr[dead_server][row] == fragment):
+			return True
+		row += 1
+	return False
 
+def getNum(dead_server, servers_quantity, servers):
+	count = 0
 
+	srv = 0
+	while (srv < servers_quantity):
+		if (srv != dead_server):
+			row = 0
+			while (row < 10):
+				if (checkFragment(servers, dead_server, servers[srv][row])):
+					count += 1
+					break
+				row += 1
+		srv += 1
+	return count
+
+def printResult(count, num):
+	res = (100.0 * count) / num 
+	print ("Killing 2 arbitrary servers results in data loss in " + str(res) + "% cases")
 
 if(sys.argv[1] == '-n'):
 	servers_quantity = int(sys.argv[2])
-	randomize(servers_quantity)
+	if(sys.argv[3] == '--random'):
+		arr = randomize(servers_quantity)
+	elif(sys.argv[3] == '--mirror'):
+		arr = mirror(servers_quantity)
+	print(arr)
+	count = getNum(0, servers_quantity, arr)
+	printResult(count, servers_quantity - 1)
 else:
 	print ('usage: ./simulate.py -n quantity[int] --method[str]')
